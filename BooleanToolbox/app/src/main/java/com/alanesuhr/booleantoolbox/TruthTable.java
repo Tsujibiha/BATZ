fb38eb36e5c23eed86bed0f5d216732c83769da0
Alane Suhr <alsuhr@gmail.com>
2014-10-04 01:14:49 -0400

    Truth Table ==> HTML. Cool stuff.

--- BooleanToolbox/app/src/main/java/com/alanesuhr/booleantoolbox/TruthTable.java
+++ BooleanToolbox/app/src/main/java/com/alanesuhr/booleantoolbox/TruthTable.java
@@ -13,11 +13,12 @@ public class TruthTable {
      * Must be of size 2^N, where N is the number of variables in the boolean expression.
      */
     public ArrayList<Boolean> values;
+    BoolExpr expr;
 
     public TruthTable(BoolExpr expr) {
         Set<BoolExpr.Variable> vars = expr.getVariablesUsed();
         int numVars = vars.size();
-        int numCombs = (int)Math.pow(2, numVars);
+        int numCombs = 1 << numVars;
 
         // Use the binary representation of all 2^N numbers to get the combinations of the various
         // variables.
@@ -34,5 +35,35 @@ public class TruthTable {
 
          values.add(expr.eval(combination));
         }
+    }
+
+    public String getHTMLTable() {
+        String html = "<table>";
+
+        // Header row
+        html += "<tr>";
+        // Each variable
+        for (BoolExpr.Variable var : expr.getVariablesUsed()) {
+            html += "<td>" + /* var to char */ + "</td>";
+        }
+        // Final expression
+        html += "<td>" + /* expr to string */ + "</td></tr>";
+
+        // Each row
+        for (int i = 0; i < values.size(); i++) {
+            html += "<tr>";
+            int digit = 0;
+            // For each variable, get the digit'th digit of the index's truth and display it.
+            for (BoolExpr.Variable var : vars) {
+                // hakcy
+                html += "<td>" + (i >> digit) & 1 != 0 ? "1" : "0") + "</td>";
+            }
+            // Expression's value
+            html += "<td>" + values[i] ? "1" : "0" + "</td>";
+            html += "</tr>";
+        }
+
+        html += "</table>";
+        return html;
     }
 }
