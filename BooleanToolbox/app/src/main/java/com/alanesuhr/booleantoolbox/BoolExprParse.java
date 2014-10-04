8e61b99c0af350ff781cf7935fdbf3ae2af4dcd0
Benjamin Fair <fair.105@osu.edu>
2014-10-03 20:56:58 -0400

    Implement BoolExprParse

--- BooleanToolbox/app/src/main/java/com/alanesuhr/booleantoolbox/BoolExprParse.java
+++ BooleanToolbox/app/src/main/java/com/alanesuhr/booleantoolbox/BoolExprParse.java
@@ -1,5 +1,7 @@
 package com.alanesuhr.booleantoolbox;
 
+import android.util.Log;
+
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -11,20 +13,82 @@ import java.util.Set;
  */
 public class BoolExprParse {
 
-    private static final String AndChars  = "*^";
-    private static final String OrChars   = "+v";
-    private static final String XorChars  = "@";
-    private static final String NotChars  = "'!";
+    private static final String TAG = "BoolExprParse";
+
+    protected static final String AndChars   = "*^";
+    protected static final String OrChars    = "+v";
+    protected static final String XorChars   = "@";
+    protected static final String NotChars   = "'!";
+    protected static final String TrueChars  = "T1";
+    protected static final String FalseChars = "F0";
+    protected static final String VarChars   = "ABCDWXYZ";
 
     BoolExpr parseExpr(Queue<Character> in) {
-        char next = in.remove();
-        boolean inverted = false;
-        if(NotChars.indexOf(next) != -1) { // starts with not
-            inverted = true;
-            next = in.remove();
+        assert !in.isEmpty();
+
+        boolean inverted = parseNot(in);
+
+        BoolExpr cur_term = parseTerm(in);
+        while(!in.isEmpty()) { // or each term
+            char next = in.remove();
+            if (OrChars.indexOf(next) != -1) {
+                BoolExpr next_term = parseTerm(in);
+                cur_term = BoolExpr.makeOr(cur_term, next_term, inverted);
+            }
         }
+        return cur_term;
+    }
 
-        if(OrChars.indexOf(in.peek()) != -1) { // has an or
+    BoolExpr parseTerm(Queue<Character> in) {
+        boolean inverted = parseNot(in);
 
+        BoolExpr cur_factor = parseFactor(in);
+        while(!in.isEmpty()) {
+            char next = in.remove();
+            if(AndChars.indexOf(next) != -1) {
+                BoolExpr next_factor = parseFactor(in);
+                cur_factor = BoolExpr.makeAnd(cur_factor, next_factor, inverted);
+            }
+        }
+        return cur_factor;
+    }
+
+    BoolExpr parseFactor(Queue<Character> in) {
+        BoolExpr factor;
+        if(in.peek() == '(') {
+            factor = parseExpr(in);
+            in.remove(); // throw out ')'
+        } else {
+            factor = parseConstOrVar(in);
+        }
+        return factor;
+    }
+
+    boolean parseNot(Queue<Character> in) {
+        boolean inverted = false;
+        if(!in.isEmpty() && NotChars.indexOf(in.peek()) != -1) { // starts with not
+            inverted = true;
+            in.remove();
+        }
+        return inverted;
+    }
+
+    BoolExpr parseConstOrVar(Queue<Character> in) {
+        boolean inverted = parseNot(in);
+
+        char c = in.remove();
+        if(TrueChars.indexOf(c) != -1) {
+            return BoolExpr.makeConst(true);
+        } else if(FalseChars.indexOf(c) != -1) {
+            return BoolExpr.makeConst(false);
+        } else {
+            int var = VarChars.indexOf(c);
+            if(var != -1) {
+                return BoolExpr.makeVar(BoolExpr.Variable.values()[var], inverted);
+            } else {
+                Log.e(TAG, "Expected Const or Var, got: " + c);
+                return null;
+            }
+        }
     }
 }
