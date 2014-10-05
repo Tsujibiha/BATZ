13a2330f13d496bde575caa9eeedc8aef2e5a8e8
Alane Suhr <alsuhr@gmail.com>
2014-10-05 00:15:28 -0400

    NOR

--- BooleanToolbox/app/src/main/java/com/alanesuhr/booleantoolbox/BoolExprManipulation.java
+++ BooleanToolbox/app/src/main/java/com/alanesuhr/booleantoolbox/BoolExprManipulation.java
@@ -31,9 +31,38 @@ public class BoolExprManipulation {
         } else {
             result = useNANDOnly(originalExpr);
         } // else if originalExpr is inverted
-            // replace originalExpr with a nand gate whose children are the original expression
+        // replace originalExpr with a nand gate whose children are the original expression
         // Also remove not-not parent-child relations
         // Also remove inverted-only children
+
+        return result;
+    }
+
+    public static BoolExpr useNOROnly(BoolExpr originalExpr) {
+        BoolExpr result;
+
+        if (originalExpr.getKind() == BoolExpr.Kind.CONST
+            || originalExpr.getKind() == BoolExpr.Kind.VAR
+            || originalExpr.getKind() == BoolExpr.Kind.BUF) {
+            result = originalExpr;
+        } else if (originalExpr.getKind() == BoolExpr.Kind.OR) {
+            // Modify the and gate
+            originalExpr.setInverted(true);
+            result = BoolExpr.makeNot(originalExpr);
+            originalExpr.setChildA(useNOROnly(originalExpr.getChildA()));
+            originalExpr.setChildB(useNOROnly(originalExpr.getChildB()));
+        } else if (originalExpr.getKind() == BoolExpr.Kind.AND) {
+            // Modify the or gate
+            BoolExpr childA = useNOROnly(originalExpr.getChildA());
+            BoolExpr childB = useNOROnly(originalExpr.getChildB());
+
+            childA.setInverted(true);
+            childB.setInverted(true);
+
+            result = BoolExpr.makeOr(childA, childB, true);
+        } else {
+            result = useNOROnly(originalExpr);
+        } 
 
         return result;
     }
